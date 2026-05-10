a18db1244367cf6720b8432017a8ce686770db4f
yasindumalmith <yasindumalmith058@gmail.com>
2026-05-10 12:24:05 +0530

    change eks dependency and config

--- DevFlow-api/src/main/java/com/example/devflowapi/config/KubernetesConfig.java
+++ DevFlow-api/src/main/java/com/example/devflowapi/config/KubernetesConfig.java
@@ -2,11 +2,18 @@ package com.example.devflowapi.config;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KubernetesConfig {
+
+    @Value("${devflow.aws.region}")
+    private String region;
+
+    @Value("${devflow.aws.cluster-name}")
+    private String clusterName;
 
     @Bean
     public KubernetesClient kubernetesClient() {
